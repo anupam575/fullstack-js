ec6521ee7f9542a61485b7fa064c14db407acdc3
anupam575 <tripathianupam208@gmail.com>
2025-06-19 00:53:57 +0530

    Update indexs.js

--- javascript coding/Promise/asyncawait/indexs.js
+++ javascript coding/Promise/asyncawait/indexs.js
@@ -1,3 +1,8 @@
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+async function fetchData() {
+  try {
+    const response = await axios.get(url); // यहाँ JS रुकेगी, जब तक response नहीं आएगा
+    console.log(response.data); // फिर ही यह चलेगा
+  } catch (error) {
+    console.error(error.message);
+  }
 }
